d1c919bf170eefa852478cc31120861fea1ca690
Antti Rasmus <antti.rasmus@gmail.com>
2014-05-08 20:20:13 -0400

    Add ECA model and test runner

---
+++ eca.py
@@ -0,0 +1,433 @@
+import numpy as np
+import time as t
+
+DEBUG_INFO = False
+
+
+def rearrange_for_plot(w):
+    """
+    Helper for tiling 1-dimensional square vectors into an array of images
+    """
+    ratio = 2/1
+    image_dim = np.sqrt(w.shape[0])
+    if image_dim - np.floor(image_dim) > 0.001:
+        print 'Chosen weights probably not representing a square image'
+        return w
+    image_dim = int(image_dim)
+    l = np.int(np.sqrt(w.shape[1] * ratio)) + 1
+    h = np.max([l / ratio, 2])
+    #print l, h
+    w_ = w.T.reshape(w.shape[1], image_dim, image_dim)
+    # TODO: A mess. Fix and clean up. Currently loses plots of some neurons!
+
+    rows = np.vstack([np.hstack(w_[l * i:l * (i + 1), :, :]) for i in
+                      range(h-1)])
+    #print np.array(rows.shape) / 28
+    if l * (h) < w_.shape[0] - 1:
+        last_row = np.hstack(w_[l * (h - 1):, :, :])
+        last_row = np.hstack((last_row, np.zeros((last_row.shape[0],
+                                                  rows.shape[1] -
+                                                  last_row.shape[1]))))
+        rows = np.vstack((rows, last_row))
+    return rows
+
+
+def lerp(t, old, new):
+    """
+    Return new interpolated value and a relative difference
+    """
+    return ((1 - 1 / t) * old + 1 / t * new,
+            (old - new) / (old + 1e-10))
+
+
+class State(object):
+    """ Object that represents any kind of state U, X, X_y, z, ...
+    """
+    def __init__(self, n, k):
+        rng = np.random.RandomState(0)
+        self.value = rng.uniform(size=(n, k))
+        #self.value = np.zeros((n, k))
+        self.k = k
+        self.n = n
+
+    def variance(self):
+        return np.average(np.log(np.var(self.value, axis=1)))
+
+    def energy(self):
+        return np.average(np.square(self.value), axis=1)
+
+
+class Model(object):
+    def __init__(self, name, (m, n), nonlin=None, identity=False):
+        rng = np.random.RandomState(0)
+        self.m = m  # input state size
+        self.n = n  # latent space size
+        self.nonlin = nonlin
+        self.identity = identity
+        # This list holds all the fast state information the model has.
+        # Because the model can be used with data of various lengths k, and
+        # data sets, different data instances are put here so that 0
+        # corresponds to the training data. Indices 1- are available for the
+        # model user
+        self.X = []
+        if identity:
+            assert self.m == self.n, "Identity phi requires matching dimensions"
+            self.E_XU = np.identity(self.n)
+        else:
+            self.E_XU = rng.uniform(size=(n, m)) - 0.5
+        self.E_XX = np.identity(n)
+        self.Q = np.identity(n)
+        self.phi = np.dot(self.Q, self.E_XU).T
+        self.child = None
+        self.parent = None
+        self.name = name
+
+    def delete_state(self):
+        assert len(self.X) > 1, "Cannot delete default state"
+        # Discard the last one
+        self.X = self.X[:-1]
+
+    def create_state(self, k):
+        self.X.append(State(self.n, k))
+        # Return the index of the created state
+        return len(self.X) - 1
+
+    def connect_parent(self, parent):
+        self.info('has parent %s' % parent.name)
+        assert self.m == parent.n, "Child dimensionality doesn't match"
+        assert self.child is None
+        assert self.parent is None
+        self.parent = parent
+        parent.child = self
+
+    def update_model(self, state_index, tau):
+        # Input layer has identity phi, so no action
+        if self.identity:
+            return
+
+        x = self.X[state_index]
+        x_prev = self.parent.X[state_index]
+        assert x_prev.k == x.k, "Sample size mismatch"
+        assert x_prev.n == self.m, "Input dim mismatch"
+        assert x.n == self.n, "Output dim mismatch"
+        assert not self.identity, "Trying to update identity layer, probably input"
+        k = x.k
+        (self.E_XU, d1) = lerp(tau,
+                               self.E_XU,
+                               np.dot(x.value, x_prev.value.T) / k)
+
+        (self.E_XX, d2) = lerp(tau,
+                               self.E_XX,
+                               np.dot(x.value, x.value.T) / k)
+        b = 1.
+        d = np.diagonal(self.E_XX)
+        np.fill_diagonal(self.Q, b / np.where(d < 0.05, 0.05, d))
+
+        # TODO: optional spatial neighborhood coupling
+        self.phi = np.dot(self.Q, self.E_XU).T
+
+        self.info('Updating model, avg(diag(E_XX))=%.2f, ' % np.average(d) +
+                  'avg phi col norm %.2f' % np.average(np.sqrt(np.sum(np.square(self.phi),
+                                                                       axis=0))))
+
+        return (d1, d2)
+
+    def update_state(self, state_index, input, tau):
+        x = self.X[state_index]
+        assert input is None or x.k == input.shape[1], "Sample size mismatch"
+
+        # If there is a layer following thise, get feedback
+        if self.child:
+            feedback = self.child.get_feedback(state_index, self)
+        else:
+            feedback = 0.
+
+        # Feedforward originates from previous layer's state or given input
+        if input is None:
+            assert not self.identity
+            input = self.parent.X[state_index].value
+            origin = self.parent.name
+        else:
+            origin = 'system input (%d dim)' % input.shape[0]
+
+        #self.info('Updating state[%d]: feedfwd from %s and feedback from %s' %
+                  #(state_index, origin, self.child.name if self.child else "nowhere"))
+
+        feedforward = np.dot(self.phi.T, input)
+
+        # Apply nonlinearity to feedforward path only
+        if self.nonlin:
+            feedforward = self.nonlin(feedforward)
+
+        new_value = feedforward - feedback
+
+        # Or if we want to apply nonlinearity to the result
+        #if self.nonlin:
+            #new_value = self.nonlin(new_value)
+
+        (x.value, d) = lerp(tau, x.value, new_value)
+        return d
+
+    def get_feedback(self, state_index, parent):
+        return np.dot(self.phi, self.X[state_index].value)
+
+    def info(self, str):
+        if DEBUG_INFO:
+            print '%5s:' % self.name, str
+
+
+class CCAModel(Model):
+    def __init__(self, name, (m, n)):
+        self.E_ZZ = []
+        super(CCAModel, self).__init__(name, (m, n), nonlin=None,
+                                       identity=False)
+        # Phi of this layer is not in use, mark it as nan
+        self.phi = np.zeros((1, 1))
+
+    def update_model(self, state_index, tau):
+        """ There is no global model state for CCA layer, instead, everything
+        happens in update_state
+        """
+        return
+
+    def create_state(self, k):
+        # Create a stack of E_ZZ in addition to base class implementation
+        self.E_ZZ.append(State(1, 1))
+        return super(CCAModel, self).create_state(k)
+
+    def delete_state(self):
+        # Discard the last one
+        self.E_ZZ = self.E_ZZ[:-1]
+        return super(CCAModel, self).delete_state()
+
+    def update_state(self, i, input, tau):
+        z = self.X[i]
+        E_ZZ = self.E_ZZ[i]
+        assert input is None, "CCA state cannot use input"
+        assert self.child is None, 'CCA cannot have children'
+        assert len(self.parent) == 2, 'CCA should have exactly 2 parents'
+        assert z.n == self.n, "Output dim mismatch"
+
+        # Update state-specific E_ZZ and calculate q for z update
+        (E_ZZ.value, di) = lerp(tau * 3.,
+                                E_ZZ.value,
+                                np.dot(z.value, z.value.T) / z.k)
+        assert E_ZZ.value.shape == (1, 1), 'E_ZZ is not a scalar!?'
+        b = 1.
+        q = b / np.max([0.05, E_ZZ.value[0, 0]])
+
+        # Update z
+        [x1, x2] = [self.parent[j].X[i].value for j in (0, 1)]
+        new_value = q * np.sum(x1 * x2, axis=0)
+
+        (z.value, dz) = lerp(tau, z.value, new_value)
+
+        self.E_XU = None
+        return np.max((np.abs(di), np.average(np.abs(dz))))
+
+    def get_feedback(self, state_index, parent):
+        assert len(self.parent) == 2, 'CCA should have exactly 2 parents'
+        # XXX: Uncomment the following to avoid signal propagation
+        # back from CCA layer
+        #return np.zeros(())
+
+        z = self.X[state_index]
+        # Find the other branch connecting to this z
+        x_other = list(set(self.parent) - set([parent]))[0]
+
+        # This is roughly: x_u_delta = self.Q * x_y * z
+        phi = self.Q * x_other.X[state_index].value
+        return phi * z.value
+
+    def connect_parent(self, parent):
+        self.info('has parent %s' % parent.name)
+        assert self.m == parent.n, "Child dimensionality doesn't match"
+        assert self.child is None
+        self.parent = parent if self.parent is None else [self.parent, parent]
+        parent.child = self
+
+
+class ECA(object):
+    """
+    Constructs chains of layers connected together. Each layer contains phi
+    matrix and corresponding state vector X.
+
+    Typical loop u -- U -- X would represented by two of these layers; one for
+    U and another one for X.
+
+    U branch would look something like this, where --- indicates state
+    and /\\/ mapping:
+
+      ------------  u
+
+      //|\//\\/\|/  phi_0 = identity
+     -------------- layer x0 = U = phi_1^T x-1 - phi_1 X1 = u - phi_1 X1
+
+      //|\//\\/\|/  phi_1 (learned mapping)
+     -------------- layer x1 = phi_1^T x0 - phi_2 x2
+
+      //|\//\\/\|/  phi_2 (learned mapping)
+     -------------- layer x2 = phi_2^T x1 - phi_3 x3
+          .
+          .
+          .
+    """
+
+    def __init__(self, n_layers, n_u, n_y, nonlin=None):
+        # 1. Create layers for u branch
+        n_all = [n_u] + n_layers
+        print 'Creating', len(n_all), 'layers with n in', n_all, 'including input layer U'
+
+        # First the input layer U
+        self.l_U = Model('U', (n_u, n_u), None, True)
+        self.layers = [self.l_U]
+
+        # Then the consecutive layers U -> X_u1 -> X_u2 -> ...
+        for i in range(len(n_all) - 1):
+            name = "X_u%d" % (i + 1)
+            self.layers.append(Model(name, (n_all[i], n_all[i + 1]), nonlin))
+            self.layers[-1].connect_parent(self.layers[-2])
+
+        last_of_u_branch = self.layers[-1]
+        self.l_Y = None
+
+        # No supervised y signal => all done!
+        if n_y is 0:
+            return
+
+        # 2. Create layers for y branch if still here
+        # Keep y branch shorter by only taking the last layer dimension
+        n_all = [n_y] + [n_layers[-1]]
+        print 'Creating', len(n_all), 'layers with n in', n_all, 'including input layer Y'
+
+        # First the input layer Y
+        self.l_Y = Model('Y', (n_y, n_y), None, True)
+        self.layers += [self.l_Y]
+
+        # Then the consecutive layers Y -> X_y1 -> X_y2 -> ...
+        for i in range(len(n_all) - 1):
+            name = "X_y%d" % (i + 1)
+            self.layers.append(Model(name, (n_all[i], n_all[i + 1]), nonlin))
+            self.layers[-1].connect_parent(self.layers[-2])
+
+        # Finally add CCA layer to connect u and y branches:
+        # ... -> X_uN -> z
+        # ... -> X_yN --'
+        print 'Creating CCA layer %d -> %d' % (n_all[-1], 1),
+        print 'with parents:', self.layers[-2].name, last_of_u_branch.name
+        self.cca = CCAModel('z', (n_all[-1], 1))
+        self.layers += [self.cca]
+        self.cca.connect_parent(self.layers[-2])
+        self.cca.connect_parent(last_of_u_branch)
+
+    def update(self, u, y, tau):
+        """
+        Performs update round-trip from u to X and back.
+        """
+        # Create default state now when we know the sample count
+        if len(self.l_U.X) == 0:
+            for l in self.layers:
+                l.create_state(u.shape[1])
+
+        self.update_states(0, u, y, tau)
+        self.update_models(0, tau)
+
+    def update_models(self, i, tau):
+        for l in self.layers:
+            l.update_model(i, tau)
+
+    def update_states(self, i, u, y, tau):
+        max_diff = -1e100
+
+        # Loop over net inputs
+        for (x, val) in zip([self.l_U, self.l_Y], [u, y]):
+            # Loop through the chain of layers
+            while x:
+                d = x.update_state(i, val, tau)
+                x = x.child
+                val = None  # Only the first layer will have value in
+
+                # For convergence detection
+                max_diff = np.max([max_diff, np.average(np.abs(d))])
+        return max_diff
+
+    def converge_state(self, i, u, y, tau):
+        max_delta = 1.0
+        iter = 0
+        (delta_limit, time_limit, iter_limit) = (1e-3, 20, 100)
+        t_start = t.time()
+        # Convergence condition, pretty arbitrary for now
+        while max_delta > delta_limit and t.time() - t_start < time_limit:
+            max_delta = self.update_states(i, u, y, tau)
+            iter += 1
+            if iter >= iter_limit:
+                break
+        print 'Converged in', "%.1f" % (t.time() - t_start), 's,', iter,
+        print 'iters, delta %.4f' % max_delta,
+        print 'Limits: i:', iter_limit, 't:', time_limit, 'd:', delta_limit
+
+    def predict_Y(self, u):
+        k = u.shape[1]
+        y = np.zeros((self.l_Y.m, k)) if self.l_Y else None
+        # Create a temporary state
+        for l in self.layers:
+            i = l.create_state(k)
+        self.converge_state(i, u, y, tau=10.)
+
+        # Take a copy, and clean up the temporary state
+        val = self.l_Y.X[i].value.copy()
+        for l in self.layers:
+            l.delete_state()
+        return val
+
+    def converged_X(self, u):
+        k = u.shape[1]
+        y = np.zeros((self.l_Y.m, k)) if self.l_Y else None
+        # Create a temporary state
+        for l in self.layers:
+            i = l.create_state(k)
+        assert i > 0, '0 is for training data'
+        self.converge_state(i, u, y, tau=5.)
+
+        # Take a copy, and clean up the temporary state
+        val = self.latent_layer(i).copy()
+        for l in self.layers:
+            l.delete_state()
+        return val
+
+    def fit_classifier(self, classifier, y):
+        classifier.fit(self.latent_layer(0).T, y)
+
+    def latent_layer(self, i):
+        """
+        Finds the latent representation for classification. The last or one
+        before CCA.
+        """
+        l = self.l_U
+        while l.child and not isinstance(l.child, CCAModel):
+            l = l.child
+        return l.X[i].value
+
+    def phi_im(self, index):
+        # index is omitted for now, and the lowest layer is plotted
+        return rearrange_for_plot(self.l_U.child.phi)
+
+    def variance(self, states=None):
+        f = lambda l: (l.name, l.X[0].variance())
+        return map(f, self.layers)
+
+    def energy(self):
+        f = lambda l: (l.name, np.diagonal(l.E_XX))
+        return map(f, self.layers)
+
+    def avg_levels(self):
+        f = lambda l: (l.name, np.linalg.norm(np.average(l.X[0].value, axis=1)))
+        return map(f, self.layers)
+
+    def phi_norms(self):
+        f = lambda l: (l.name, np.average(np.linalg.norm(l.phi, axis=0)))
+        return map(f, self.layers)
+
+    def reconstruction_error(self):
+        # TODO
+        pass
---
+++ test_runner.py
@@ -0,0 +1,278 @@
+#!/usr/bin/env python
+
+import cPickle
+import gzip
+import os
+import time
+import matplotlib.pyplot as plt
+import matplotlib.cm as cm
+import numpy as np
+from eca import ECA
+
+conf = {
+    'use_minibatches': False,
+    'use_svm': False
+}
+
+if conf['use_svm']:
+    from sklearn import svm
+else:
+    from sklearn.linear_model import LogisticRegression
+
+
+class Data(object):
+    """
+    Class for handling training, validation, and test data
+    """
+    def __init__(self, batch_size=500, testset_size=10000):
+        # Download e.g. from http://deeplearning.net/data/mnist/mnist.pkl.gz
+        filename = 'mnist.pkl.gz'
+        if not os.path.exists(filename):
+            raise Exception("Dataset not found, please run:\n  wget http://deeplearning.net/data/mnist/mnist.pkl.gz")
+
+        data = cPickle.load(gzip.open(filename))
+        self.batch_size = batch_size
+        self.testset_size = testset_size
+        self.data = {
+            'trn': (np.float32(data[0][0]), np.int32(data[0][1])),
+            'val': (np.float32(data[1][0][:testset_size]),
+                    np.int32(data[1][1][:testset_size])),
+            'tst': (np.float32(data[2][0][:testset_size]),
+                    np.int32(data[2][1][:testset_size]))
+        }
+
+    def size(self, type, i=None, as_one_hot=False):
+        assert type in self.data.keys(), 'type has to be in %s' % str(self.data.keys())
+        y_dim = 10 if as_one_hot else 1
+        batch_size = self.data[type][0].shape[0] if i is None else self.batch_size
+        return ((self.data[type][0].shape[1], batch_size), (y_dim, batch_size))
+
+    def get(self, type, i=None, as_one_hot=False, limit=None):
+        """
+        Returns a tuple (u, y) of i'th minibatch expanded into a one-hot coded vectors if necessary.
+
+        E.g. 5 -> [0, 0, 0, 0, 0, 1, 0, 0, 0, 0]
+        """
+        assert type in self.data.keys(), 'type has to be in %s' % str(self.data.keys())
+        assert i is None or (i + 1) * self.batch_size <= len(self.data[type][0])
+        (u, y) = self.data[type]
+
+        batch_size = u.shape[0] if i is None else self.batch_size
+        batch_size = batch_size if limit is None else min(batch_size, limit)
+        i = 0 if i is None else i
+
+        u = u[(i * batch_size):((i + 1) * batch_size), :].T
+
+        if not as_one_hot:
+            return (u, y[(i * batch_size):((i + 1) * batch_size)].T)
+
+        # Convert into one_hot presentation 2 -> [0, 0, 1, 0, ...]
+        y_ = np.array(np.zeros(10))
+        y_[y[i * batch_size]] = 1.
+        for i in range(i * batch_size + 1, (i + 1) * batch_size):
+            new = np.zeros(10)
+            new[y[i]] = 1.
+            y_ = np.vstack((y_, new))
+        y_ = np.float32(y_)
+        return (u, y_.T)
+
+
+def get_optimal_system_size(input):
+    singulars = np.linalg.svd(input)[1]
+    for i in range(1, len(singulars)):
+        if np.square(singulars[i]) < np.square(np.sum(singulars[:i]) / ((i + 1) * (1 + 1) - 1)):
+            print 'Optimal n for linear system would be', i
+            break
+
+
+class TestCaseBase(object):
+    def __init__(self):
+        self.batch_size = 1000  # Actually training input size (k)
+        self.testset_size = 1000
+        self.data = Data(batch_size=self.batch_size, testset_size=self.testset_size)
+        self.trn_iters = 400
+        self.mdl = None
+        self.onehot = False
+        self.tau_start = None
+        self.tau_end = None
+        self.tau_alpha = None
+
+        self.configure()
+        assert self.mdl is not None
+
+        (u, y) = self.data.get('trn', i=0, as_one_hot=self.onehot)
+        #get_optimal_system_size(u)
+
+        # This is global slowness that helps the model to get past the
+        # unstable beginning..
+        tau = self.tau_start
+        if self.onehot:
+            y *= 9.2
+            print 'energy u:', np.average(np.sum(np.square(u), axis=0)),
+            print 'energy y:', np.average(np.sum(np.square(y), axis=0))
+
+        print 'Training...'
+        try:
+            t = time.time()
+            for i in range(self.trn_iters):
+                # This does not work. Visualization of FI * X becomes blurred (an
+                # average of samples?)
+                if conf['use_minibatches']:
+                    assert False, "Not supported"
+                    u, y = self.data.get('trn', i=0)
+
+                self.mdl.update(u, y, tau)
+                tau = tau * self.tau_alpha + (1 - self.tau_alpha) * self.tau_end
+
+                i_str = "I %4d:" % (i + 1)
+                # Progress prings
+                if ((i + 1) % 20 == 0):
+                    t_str = 't: %.2f s' % (time.time() - t)
+                    t = time.time()
+                    tau_str = "Tau: %4.1f" % tau
+
+                    tostr = lambda t: "{" + ", ".join(["%s: %6.2f" % (n, v) for (n, v) in t]) + "}"
+
+                    var_str = " logvar:" + tostr(self.mdl.variance())
+                    a_str   = " avg:   " + tostr(self.mdl.avg_levels())
+                    phi_str = " |phi|: " + tostr(self.mdl.phi_norms())
+
+                    print i_str, tau_str, t_str, var_str, phi_str
+                    #print var_str, a_str, phi_str
+                    #print var_str, phi_str
+
+                # Accuracy prints
+                if ((i + 1) % 200 == 0):
+                    t2 = time.time()
+                    (trn_acc, val_acc) = self.calculate_accuracy(u, y)
+                    t_str = 't: %.2f s,' % (time.time() - t2)
+                    acc_str = "Accuracy trn %6.2f %%, val %6.2f %%" % (100. * trn_acc,
+                                                              100. * val_acc)
+
+                    print i_str, acc_str, t_str
+
+            print 'Testing accuracy...'
+            (ut, yt) = self.data.get('tst', as_one_hot=self.onehot)
+            # TODO: test error broken at the moment, trust validation as it is not used for
+            # hyper-parameter tuning at the moment
+            #tst_acc = self.calculate_accuracy(ut, yt)
+            tst_acc = np.nan
+            print "Accuracy: Training %6.2f %%, validation %6.2f %%, test %6.2f %%" % (
+                100. * trn_acc,
+                100. * val_acc,
+                100. * tst_acc)
+        except KeyboardInterrupt:
+            pass
+        # Viz
+        # Plot some energies ("lambdas")
+        #e = np.array(self.mdl.energy())
+        #f, ax = plt.subplots(len(e))
+        #for (i, row) in enumerate(e):
+            #ax[i].plot(np.sort(row)[::-1])
+            #print "E(X%d)" % i, ", ".join(["%4.2f" % v for v in np.sort(row)[-1:-3:-1]]), '...', \
+                #", ".join(["%4.2f" % v for v in np.sort(row)[0:3]])
+
+        #plt.show()
+        self.visualize()
+
+    def configure(self):
+        raise NotImplemented()
+
+    def accuracy(self, y_est, y_true):
+        y_true = y_true if len(y_true.shape) == 1 else np.argmax(y_true, axis=0)
+        return float(np.bincount(y_est == y_true, minlength=2)[1]) / len(y_est)
+
+    def calculate_accuracy(self, u, y):
+        if conf['use_svm']:
+            classifier = svm.LinearSVC()
+            print 'Testing accuracy with SVM...'
+        else:
+            classifier = LogisticRegression()
+            print 'Testing accuracy with logistic regression...'
+
+        # y has the corresponding labels to the data used in teaching
+        # so we can build the model now
+        self.mdl.fit_classifier(classifier, y)
+
+        def estimate(data):
+            state = self.mdl.converged_X(data)
+            y_est = classifier.predict(state.T)
+            return y_est
+
+        y_est = estimate(u[:, :self.testset_size])
+        trn_acc = self.accuracy(y_est, y.T[:self.testset_size].T)
+        (uv, yv) = self.data.get('val', limit=self.batch_size, as_one_hot=self.onehot)
+        val_acc = self.accuracy(estimate(uv), yv)
+        return (trn_acc, val_acc)
+
+rect = lambda x: np.where(x < 0., 0., x)
+
+class UnsuperLayer(TestCaseBase):
+    def configure(self):
+        layers = [70]  # Try e.g. [30, 20] for multiple layers and increase tau start
+        self.tau_start = 20
+        self.tau_end = 5
+        self.tau_alpha = 0.99
+        self.mdl = ECA(layers,
+                       self.data.size('trn', 0)[0][0],
+                       0,  # n of output
+                       np.abs) # np.tanh, rect, None, etc..
+
+    def visualize(self):
+        plt.imshow(self.mdl.phi_im(1), cmap=cm.Greys_r)
+        plt.show()
+
+
+class SuperLayer(TestCaseBase):
+    def configure(self):
+        layers = [30]
+        self.tau_start = 60
+        self.tau_end = 5
+        self.tau_alpha = 0.99
+        self.onehot = True
+        self.mdl = ECA(layers,
+                       self.data.size('trn', 0)[0][0],
+                       self.data.size('trn', 0, as_one_hot=True)[1][0],
+                       rect)
+
+    def visualize(self):
+        plt.imshow(self.mdl.phi_im(1), cmap=cm.Greys_r)
+        plt.show()
+
+    def calculate_accuracy(self, u, y):
+        # Use base class implementation, because CCA regression doesn't work?
+        #return super(SuperLayer, self).calculate_accuracy(u, np.argmax(y, axis=0))
+
+        # TODO: This CCA does not give sensible y out, figure out why
+        print 'Testing accuracy...'
+
+        def estimate(data, y):
+            prediction = self.mdl.predict_Y(data)
+            # Print what the two first samples give
+            #for i in range(2):
+                #print prediction.T[i], np.argmax(prediction.T[i]), np.argmax(y.T[i])
+            return np.argmin(prediction, axis=0)
+
+        (ut, yt) = (u[:, :self.batch_size], y.T[:self.batch_size].T)
+        y_est = estimate(ut, yt)
+        trn_acc = self.accuracy(y_est, yt)
+        (uv, yv) = self.data.get('val', limit=self.batch_size, as_one_hot=self.onehot)
+        val_acc = self.accuracy(estimate(uv, yv), yv)
+        return (trn_acc, val_acc)
+
+
+def main():
+    print 'Initializing...'
+
+    UnsuperLayer()
+
+    # CCA experiment
+    #SuperLayer()
+
+    #print 'super 81.68 %%' # Some older results, cannot remember hyper-params
+    #print 'one l 88.29 %%'
+    #print 'two l 89.42 %%'
+
+
+if __name__ == '__main__':
+    main()
