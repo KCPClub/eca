9849ad007b25226cf6bbb83dbb4d02975ae3a763
Antti Rasmus <antti.rasmus@gmail.com>
2014-05-12 23:35:47 -0400

    Clean up mnist runner

--- experiments/mnist_classification/run.py
+++ experiments/mnist_classification/run.py
@@ -7,26 +7,13 @@ import theano.tensor as T
 from eca import ECA
 from utils import MnistData
 
-conf = {
-    'use_minibatches': False,
-    'use_svm': False
-}
-
-if conf['use_svm']:
-    from sklearn import svm
-else:
-    from sklearn.linear_model import LogisticRegression
-
 
 class TestCaseBase(object):
     def __init__(self):
-        self.batch_size = 10000  # Actually training input size (k)
         self.testset_size = 1000
-        self.data = MnistData(batch_size=self.batch_size, testset_size=self.testset_size)
+        self.data = MnistData(batch_size=100, testset_size=self.testset_size)
         self.trn_iters = 400
         self.mdl = None
-        self.onehot = False
-        self.y_in_u = False
         self.tau_start = None
         self.tau_end = None
         self.tau_alpha = None
@@ -34,28 +21,16 @@ class TestCaseBase(object):
         self.configure()
         assert self.mdl is not None
 
-        (u, y) = self.data.get('trn', i=0, as_one_hot=self.onehot)
+        (u, y) = self.get_data('trn')
 
         # This is global slowness that helps the model to get past the
         # unstable beginning..
         tau = self.tau_start
-        if self.onehot:
-            y *= 9.2
-            print 'energy u:', np.average(np.sum(np.square(u), axis=0)),
-            print 'energy y:', np.average(np.sum(np.square(y), axis=0))
-
-        if self.y_in_u:
-            u = np.vstack([u, y])
 
         print 'Training...'
         try:
             t = time.time()
             for i in range(self.trn_iters):
-                # This does not work. Visualization of FI * X becomes blurred (an
-                # average of samples?)
-                if conf['use_minibatches']:
-                    assert False, "Not supported"
-                    u, y = self.data.get('trn', i=0)
 
                 self.mdl.update(u, y, tau)
                 tau = tau * self.tau_alpha + (1 - self.tau_alpha) * self.tau_end
@@ -88,7 +63,6 @@ class TestCaseBase(object):
                     print i_str, acc_str, t_str
 
             print 'Testing accuracy...'
-            (ut, yt) = self.data.get('tst', as_one_hot=self.onehot)
             # TODO: test error broken at the moment, trust validation as it is not used for
             # hyper-parameter tuning at the moment
             #tst_acc = self.calculate_accuracy(ut, yt)
@@ -99,17 +73,6 @@ class TestCaseBase(object):
                 100. * tst_acc)
         except KeyboardInterrupt:
             pass
-        # Viz
-        # Plot some energies ("lambdas")
-        #e = np.array(self.mdl.energy())
-        #f, ax = plt.subplots(len(e))
-        #for (i, row) in enumerate(e):
-            #ax[i].plot(np.sort(row)[::-1])
-            #print "E(X%d)" % i, ", ".join(["%4.2f" % v for v in np.sort(row)[-1:-3:-1]]), '...', \
-                #", ".join(["%4.2f" % v for v in np.sort(row)[0:3]])
-
-        #plt.show()
-        #self.visualize()
 
     def configure(self):
         raise NotImplemented()
@@ -119,52 +82,98 @@ class TestCaseBase(object):
         y_est = y_est if len(y_est.shape) == 1 else np.argmax(y_est, axis=0)
         return float(np.bincount(y_est == y_true, minlength=2)[1]) / len(y_est)
 
+rect = lambda x: np.where(x < 0., 0., x)
+
+class UnsupervisedLearning(TestCaseBase):
+    def configure(self):
+        layers = [20]  # Try e.g. [30, 20] for multiple layers and increase tau start
+        self.tau_start = 30
+        self.tau_end = 5
+        self.tau_alpha = 0.99
+        self.mdl = ECA(layers,
+                       self.data.size('trn', 0)[0][0] + 10,
+                       0,  # n of output
+                       T.abs_)  # np.tanh, rect, None, etc..
+
+
+    def get_data(self, type):
+        assert type in ['tst', 'trn', 'val']
+
+        (u, y) = self.data.get(type, i=0, as_one_hot=True)
+        # Equalize energies
+        u_avg_en = np.average(np.sum(np.square(u), axis=0))
+        y_avg_en = np.average(np.sum(np.square(y), axis=0))
+        y *= np.sqrt(u_avg_en / y_avg_en)
+
+        if type == 'trn':
+            u = np.vstack([u, y])
+        else:
+            u = np.vstack((u, np.zeros((10, u.shape[1]), dtype=np.float32)))
+        return (u, y)
+
     def calculate_accuracy(self, u, y):
         if len(y.shape) == 2:
             y = np.argmax(y, axis=0)
-        if conf['use_svm']:
-            classifier = svm.LinearSVC()
-            print 'Testing accuracy with SVM...'
-        else:
-            classifier = LogisticRegression()
-            print 'Testing accuracy with logistic regression...'
-
-        # y has the corresponding labels to the data used in teaching
-        # so we can build the model now
-        #self.mdl.fit_classifier(classifier, y)
-        #def estimate(data):
-            #state = self.mdl.converged_X(data)
-            #y_est = classifier.predict(state.T)
-            #return y_est
 
         def estimate(data, missing=None):
             y = self.mdl.converged_U(data, missing)
             return y[-10:, :]
 
         y_est = self.mdl.uest()[-10:, :]
-        #y_est = estimate(u[:, :self.testset_size])
         trn_acc = self.accuracy(y_est[:, :self.testset_size], y.T[:self.testset_size].T)
 
-        (uv, yv) = self.data.get('val', limit=self.batch_size, as_one_hot=self.onehot)
-        if self.y_in_u:
-            uv = np.vstack((uv, np.zeros((10, uv.shape[1]), dtype=np.float32)))
+        (uv, yv) = self.get_data('val')
         val_acc = self.accuracy(estimate(uv), yv)
         return (trn_acc, val_acc)
 
-rect = lambda x: np.where(x < 0., 0., x)
+    def visualize(self):
+        import matplotlib.pyplot as plt
+        import matplotlib.cm as cm
+        plt.imshow(rearrange_for_plot(self.mdl.first_phi()[:-10, :]), cmap=cm.Greys_r)
+        plt.show()
 
-class UnsuperLayer(TestCaseBase):
+
+class SupervisedLearning(TestCaseBase):
     def configure(self):
         layers = [20]  # Try e.g. [30, 20] for multiple layers and increase tau start
         self.tau_start = 30
         self.tau_end = 5
         self.tau_alpha = 0.99
-        self.onehot = True
-        self.y_in_u = True
         self.mdl = ECA(layers,
-                       self.data.size('trn', 0)[0][0] + 10,
+                       self.data.size('trn', 0)[0][0],
                        0,  # n of output
                        T.abs_)  # np.tanh, rect, None, etc..
+
+
+    def get_data(self, type):
+        assert type in ['tst', 'trn', 'val']
+
+        (u, y) = self.data.get(type, i=0, as_one_hot=True)
+        # Equalize energies
+        u_avg_en = np.average(np.sum(np.square(u), axis=0))
+        y_avg_en = np.average(np.sum(np.square(y), axis=0))
+        y *= np.sqrt(u_avg_en / y_avg_en)
+
+        if type == 'trn':
+            u = np.vstack([u, y])
+        else:
+            u = np.vstack((u, np.zeros((10, u.shape[1]), dtype=np.float32)))
+        return (u, y)
+
+    def calculate_accuracy(self, u, y):
+        if len(y.shape) == 2:
+            y = np.argmax(y, axis=0)
+
+        def estimate(data, missing=None):
+            y = self.mdl.converged_U(data, missing)
+            return y[-10:, :]
+
+        y_est = self.mdl.uest()[-10:, :]
+        trn_acc = self.accuracy(y_est[:, :self.testset_size], y.T[:self.testset_size].T)
+
+        (uv, yv) = self.get_data('val')
+        val_acc = self.accuracy(estimate(uv), yv)
+        return (trn_acc, val_acc)
 
     def visualize(self):
         import matplotlib.pyplot as plt
@@ -175,7 +184,7 @@ class UnsuperLayer(TestCaseBase):
 
 def main():
     print 'Initializing...'
-    UnsuperLayer()
+    UnsupervisedLearning()
 
 if __name__ == '__main__':
     main()
