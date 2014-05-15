223d6b320a4a2c543aadc6d07582109c0e7fbb47
Antti Rasmus <antti.rasmus@gmail.com>
2014-05-15 09:27:03 -0400

    tools: Add more plots

--- tools/state_updates.py
+++ tools/state_updates.py
@@ -5,93 +5,124 @@ import matplotlib.pyplot as plt
 
 # Default values
 l = 0.3
-U_init = 0
-Y_init = 0
-X_init = 0
-X2_init = 0
-X3_init = 0
+U1_0 = 0
+Y1_0 = 0
+X1_0 = 0
+X2_0 = 0
+X3_0 = 0
+phi_0 = 1.
 u = 10
 y = 1
 
-
 def update_accu_1(state):
-    (U, X) = state if state else (U_init, X_init)
+    (U, X) = state if state else (U1_0, X2_0)
     U = 1*U + l*(u - 1*X)
     X = (1*U)
     return (U, X)
 
 
-def update_accu_2(state):
-    (U, X) = state if state else (U_init, X_init)
+def update_accu_both(state):
+    (U, X) = state if state else (U1_0, X1_0)
     U = 1*U + l*(u - 1*X)
     X = 1*X + l*(1*U)
     return (U, X)
 
 
-def update_interp_2(state):
-    (U, X) = state if state else (U_init, X_init)
-    U = (1-l)*U + l*(u - 1 * X)
-    X = (1-l)*X + l*(1 * U)
-    return (U, X)
+def update_accu_all_3(state):
+    (U1, X1, X2, phi1, phi2) = state if state else (U1_0, X1_0, X2_0, phi_0,
+                                                    phi_0)
+    phi1 = lerp(phi1, q(X1) * X1 * U1)
+    phi2 = lerp(phi2, q(X2) * X2 * X1)
+    U1 += l * (u - phi1*X1)
+    X1 += l * (phi1 * U1 - phi2 * X2)
+    X2 += l * (phi2 * X1)
+    return (U1, X1, X2, phi1, phi2)
 
 
-def update_accu_3(state):
-    (U, X, X2) = state if state else (U_init, X_init, X2_init)
-    U  = 1*U  + l*(u - 1*X)
-    X  = 1*X  + l*(1*U - 1 * X2)
-    X2 = 1*X2 + l*(1*X)
-    return (U, X, X2)
+def lerp(old, new):
+    return (1-l) * old + l * new
 
 
-def update_interp_3(state):
-    (U, X, X2) = state if state else (U_init, X_init, X2_init)
-    U = (1-l)*U + l*(u - 1 * X)
-    X = (1-l)*X + l*(1 * U - 1 * X2)
-    X2 = (1-l)*X2 + l*(1 * X)
-    return (U, X, X2)
+def q(X):
+    return 1. / max((X * X), 1e-10)
 
 
-def update_two_way(state):
-    (U, X, Y) = state if state else (U_init, X_init, Y_init)
-    U = (1-l)*U + l*(u - 1 * X)
-    Y = (1-l)*Y + l*(y - 1 * X)
-    X = (1-l)*X + l*(1 * U + 1 * Y)
-    return (U, X, Y)
+def update_interp_both(state):
+    (U, X, phi) = state if state else (U1_0, X1_0, phi_0)
+    phi = lerp(phi, q(X) * X*U)
+    U = lerp(U, u - phi * X)
+    X = lerp(X,     phi * U)
+    return (U, X, phi)
+
+
+def update_interp_all_3(state):
+    (U1, X1, X2, phi1, phi2) = state if state else (U1_0, X1_0, X2_0,
+                                                    phi_0, phi_0)
+    phi1 = lerp(phi1, q(X1) * X1*U1)
+    phi2 = lerp(phi2, q(X2) * X2*X1)
+
+    U1 = lerp(U1,         u - phi1 * X1)
+    X1 = lerp(X1, phi1 * U1 - phi2 * X2)
+    X2 = lerp(X2, phi2 * X1)
+    return (U1, X1, X2, phi1, phi2)
+
+
+def update_interp_all_4(s):
+    U1, X1, X2, X3, phi1, phi2, phi3 = s if s else (U1_0, X1_0, X2_0,
+                                                    X2_0, phi_0,
+                                                    phi_0, phi_0)
+    phi1 = lerp(phi1, q(X1) * X1*U1)
+    phi2 = lerp(phi2, q(X2) * X2*X1)
+    phi3 = lerp(phi3, q(X3) * X3*X2)
+
+    U1 = lerp(U1,         u - phi1 * X1)
+    X1 = lerp(X1, phi1 * U1 - phi2 * X2)
+    X2 = lerp(X2, phi2 * X1 - phi3 * X3)
+    X3 = lerp(X3, phi3 * X2)
+    return (U1, X1, X2, X3, phi1, phi2, phi3)
+
+
+def update_two_way(state, sign=1., a=1.):
+    (U, X, Y, phiU, phiY) = state if state else (U1_0, X1_0, Y1_0,
+                                                 phi_0, phi_0)
+    phiU = lerp(phiU, q(X) * X * U)
+    phiY = lerp(phiY, q(X) * X * Y)
+    U = lerp(U,        u - phiU * X)
+    Y = lerp(Y,        y - phiY * X)
+    X = lerp(X, (phiU * U + phiY * Y * sign) * a)
+    return (U, X, Y, phiU, phiY)
 
 
 def update_two_way_minus(state):
-    (U, X, Y) = state if state else (U_init, X_init, Y_init)
-    U = (1-l)*U + l*(u - 1 * X)
-    Y = (1-l)*Y + l*(y - 1 * X)
-    X = (1-l)*X + l*(1 * U - 1 * Y)
-    return (U, X, Y)
+    return update_two_way(state, -1)
 
 
-def update_interp_4(state):
-    (U, X, X2, X3) = state if state else (U_init, X_init, X2_init, X2_init)
-    U = (1-l)*U + l*(u - 1 * X)
-    X = (1-l)*X + l*(1 * U - 1 * X2)
-    X2 = (1-l)*X2 + l*(1 * X - 1 * X3)
-    X3 = (1-l)*X3 + l*(1 * X2)
-    return (U, X, X2, X3)
+def update_two_way_avg(state):
+    return update_two_way(state, a=0.5)
 
 
-def update_interp_1(state):
-    (U, X) = state if state else (U_init, X_init)
-    U = (1. - l) * U + l * (u - 1. * X)
+def update_two_way_avg_minus(state):
+    return update_two_way(state, sign=-1, a=0.5)
+
+
+def update_interp_other(state):
+    (U, X) = state if state else (U1_0, X1_0)
+    U = lerp(U, u - 1. * X)
     X = (1. * U)
     return (U, X)
 
 updates = [
-    #update_accu_one,
+    #update_accu_1,
+    #update_accu_both,
+    update_accu_all_3,
     update_two_way,
+    update_two_way_avg,
+    update_two_way_avg_minus,
     update_two_way_minus,
-    update_accu_1,
-    update_accu_3,
-    update_interp_1,
-    update_interp_2,
-    update_interp_3,
-    update_interp_4,
+    update_interp_other,
+    update_interp_both,
+    update_interp_all_3,
+    update_interp_all_4,
 ]
 
 for j in range(len(updates)):
@@ -109,7 +140,7 @@ for j in range(len(updates)):
         plt.plot(t, i)
     print 'u: %.1f' % u, 'state:',
     for k in state:
-        print '%.1f' % k,
+        print '%.2f' % k,
     print
 plt.show()
 
