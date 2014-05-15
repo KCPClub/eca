973ebde143488f85d56ddaf2e3bfe60625e164aa
Antti Rasmus <antti.rasmus@gmail.com>
2014-05-15 01:01:26 -0400

    tools: Add new plots for multi-loop cases

--- tools/state_updates.py
+++ tools/state_updates.py
@@ -3,50 +3,113 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 
-def update_accu_one(U, X):
+# Default values
+l = 0.3
+U_init = 0
+Y_init = 0
+X_init = 0
+X2_init = 0
+X3_init = 0
+u = 10
+y = 1
+
+
+def update_accu_1(state):
+    (U, X) = state if state else (U_init, X_init)
     U = 1*U + l*(u - 1*X)
     X = (1*U)
     return (U, X)
 
 
-def update_accu_both(U, X):
+def update_accu_2(state):
+    (U, X) = state if state else (U_init, X_init)
     U = 1*U + l*(u - 1*X)
     X = 1*X + l*(1*U)
     return (U, X)
 
 
-def update_interp_both(U, X):
-    U = (1-l)*U + l*(u - 1*X)
-    X = (1-l)*X + l*(1*U)
+def update_interp_2(state):
+    (U, X) = state if state else (U_init, X_init)
+    U = (1-l)*U + l*(u - 1 * X)
+    X = (1-l)*X + l*(1 * U)
     return (U, X)
 
 
-def update_interp_1(U, X):
+def update_accu_3(state):
+    (U, X, X2) = state if state else (U_init, X_init, X2_init)
+    U  = 1*U  + l*(u - 1*X)
+    X  = 1*X  + l*(1*U - 1 * X2)
+    X2 = 1*X2 + l*(1*X)
+    return (U, X, X2)
+
+
+def update_interp_3(state):
+    (U, X, X2) = state if state else (U_init, X_init, X2_init)
+    U = (1-l)*U + l*(u - 1 * X)
+    X = (1-l)*X + l*(1 * U - 1 * X2)
+    X2 = (1-l)*X2 + l*(1 * X)
+    return (U, X, X2)
+
+
+def update_two_way(state):
+    (U, X, Y) = state if state else (U_init, X_init, Y_init)
+    U = (1-l)*U + l*(u - 1 * X)
+    Y = (1-l)*Y + l*(y - 1 * X)
+    X = (1-l)*X + l*(1 * U + 1 * Y)
+    return (U, X, Y)
+
+
+def update_two_way_minus(state):
+    (U, X, Y) = state if state else (U_init, X_init, Y_init)
+    U = (1-l)*U + l*(u - 1 * X)
+    Y = (1-l)*Y + l*(y - 1 * X)
+    X = (1-l)*X + l*(1 * U - 1 * Y)
+    return (U, X, Y)
+
+
+def update_interp_4(state):
+    (U, X, X2, X3) = state if state else (U_init, X_init, X2_init, X2_init)
+    U = (1-l)*U + l*(u - 1 * X)
+    X = (1-l)*X + l*(1 * U - 1 * X2)
+    X2 = (1-l)*X2 + l*(1 * X - 1 * X3)
+    X3 = (1-l)*X3 + l*(1 * X2)
+    return (U, X, X2, X3)
+
+
+def update_interp_1(state):
+    (U, X) = state if state else (U_init, X_init)
     U = (1. - l) * U + l * (u - 1. * X)
     X = (1. * U)
     return (U, X)
 
-updates = [update_accu_one,
-           update_accu_both,
-           update_interp_both,
-           update_interp_1]
+updates = [
+    #update_accu_one,
+    update_two_way,
+    update_two_way_minus,
+    update_accu_1,
+    update_accu_3,
+    update_interp_1,
+    update_interp_2,
+    update_interp_3,
+    update_interp_4,
+]
 
-x = range(30)
 for j in range(len(updates)):
     plt.subplot(len(updates), 1, j)
     plt.title(updates[j].func_name)
-    l = 0.3
-    U = 20
-    X = 0
-    u = 10
-    y1 = []
-    y2 = []
-    for i in x:
-        U, X = updates[j](U, X)
-        print "U: %.1f, X: %.1f, u: %.2f" % (U, X, u)
-        y1.append(U)
-        y2.append(X)
-    plt.plot(x, y1)
-    plt.plot(x, y2)
+    print updates[j].func_name
+
+    t = range(30)
+    state = None
+    states = []
+    for i in t:
+        state = updates[j](state)
+        states += [state]
+    for i in np.array(states).T:
+        plt.plot(t, i)
+    print 'u: %.1f' % u, 'state:',
+    for k in state:
+        print '%.1f' % k,
+    print
 plt.show()
 
